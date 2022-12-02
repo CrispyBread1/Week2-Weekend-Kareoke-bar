7354e112b9357c2ae9db9da4e3c57185aa889338
Daniel <dsalvatori2008@gmail.com>
2022-12-02 11:27:59 +0000

    set up

---
+++ tests/room_test.py
@@ -0,0 +1,15 @@
+import unittest
+from classes.room import *
+from classes.guest import *
+from classes.song import *
+
+class TestGuest(unittest.TestCase):
+
+    def setUp(self):
+        self.guest = 
+        self.room = TestRoom
+        self.song = TestSong
+
+    
+    def test_kareoke_room_is_empty(self):
+        
